bdcdfd259fd3ab4d074e69c68c838646e6789daf
apoorvakarn <karnapoorva@gmail.com>
2018-01-16 14:08:53 +0530

    hh

---
+++ class1.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 16 12:52:47 2018 by Apoorva Karn"""
+
+
+
+#list
+x= [1,2,3]
+x
+print(x)
+l=eval(input("enter a list"))
+l
+rollnos= [1,2,3,4,5,6,10,11]
+rollnos[0]
+rollnos[8]
+rollnos[7]
+len(rollnos)
+sum(rollnos)
+min(rollnos)
+max(rollnos)
+rollnos
+for i in range (len(rollnos)):
+    print(rollnos[i],end = ',')
+if 3 in rollnos:
+    print("yes")
+if 7 in rollnos:
+    print("yes")
+else:
+    print("no")
+    
+rollnos.append(13)
+rollnos
+sorted(rollnos)
+rollnos.index(13)# for value
+rollnos.reverse
+sorted(-(rollnos))
---
+++ class_1.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 16 12:25:08 2018 by Apoorva Karn"""
+
+
+
+import numpy as np
+x=5
+y=4
+x+y
+
+
+import keyword
+print(keyword.kwlist)
+
+
+a=b=c=1
+a
+b
+c
+a,b,c = 1,2,"tom"
+a
+b
+c
+a;b;c
+a,b,c
+print(a,b,c)
+
+
+for i in range(4):
+    print(i)
+
+
+for i in range(4):
+    print(i,end=' ')
+
+
+for i in range(4):
+    print(i,end = " ")
+    print(i + 2)
+    for j in range(0):
+        print(j)
+
+
+for i in range (4):
+    print(i, i+2, sep ="&",end= "-")
+
+
+first_item= 1
+second_item = 2
+third_item = 3
+gtotal = first_item +\
+second_item + \
+third_item
+gtotal
+
+
+word = ''' apoorva karn '''
+word
+
+a=2
+print("no of students",a)
+
+
+str = input("enter your name")
+str
+
+n= input("enter a number")
+n
+m = eval(input ("enter a number"))
+m
+m*3
+""" comment"""
+n*3
+print()
+dir()
+help()
