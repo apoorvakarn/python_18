fc9b2f4352cb8ced800c7e20ba83b2203b63654c
apoorvakarn <karnapoorva@gmail.com>
2018-01-31 12:54:42 +0530

    31012018

--- firstnp.py
+++ firstnp.py
@@ -9,3 +9,24 @@ import numpy as np
 x=5
 y=4
 x+y
+ ##loops##
+ #for loop#
+l=[1,2,3]
+for i in range(len(l)):
+    print(l[i])
+
+len?
+len(l)
+help(len)
+l.insert?
+L?
+l
+def square(a):
+    return(a**2)
+square(3)
+def square(a):
+    """ this will square the value"""
+    return(a**2)
+square(3)
+square?
+str.*find*?
